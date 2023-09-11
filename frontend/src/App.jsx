ed96c1895a7d3ae7d6e433df4ba8ac258b9efa1d
ashishgupta <ashish189000@gmail.com>
2023-09-11 20:35:42 +0530

    added navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navigation from "./customer/components/navigation/Navigation";
+import "./input.css";
+function App() {
+  return (
+    <>
+      <Navigation/> 
+    </>
+  );
+}
+
+export default App;
